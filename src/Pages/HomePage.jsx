f36a91165febb4757b71c8586536ae940a484e76
Shujaat Azim <shujaat.azim@gmail.com>
2025-05-12 20:44:15 +0900

    Add description and remove no title results

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,9 +7,16 @@ const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div style={{ maxWidth: '500px' }}>
       <h3>Welcome to Hacker News Search!</h3>
       <h5>By Shujaat Azim</h5>
+      <p>Here you can search for articles on Hacker News. Simply click search and type in some terms; 
+        your history will be preserved in the History tab (until page reload).
+      </p>
+      <p>
+        This small, no-frills project was built in a half hour for a coding interview. I've made some small improvements
+        since then, but wanted to preserve the minimalistic look and feel. Enjoy!
+      </p>
       <div>
         <Button color="primary" onClick={() => navigate("/search")}>Search</Button>
         <Button color="primary" onClick={() => navigate("/history")}>History</Button>
